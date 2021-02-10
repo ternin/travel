7a9fdb86210f739016efbebc6b1d621288ff97ce
ternin <78250324+ternin@users.noreply.github.com>
2021-02-10 19:44:09 +0530

    changes made

--- travel/travel/src/Places/VirtualPlaces.jsx
+++ travel/travel/src/Places/VirtualPlaces.jsx
@@ -8,40 +8,101 @@ export function VirtualPlacesRaichur() {
             <Carousel>
   <Carousel.Item>
     <img
-      className="d-block w-100"
-      src="https://cdn.pixabay.com/photo/2020/08/02/11/20/chennai-to-tirupati-package-5457397__480.jpg"
+      src="https://www.gosahin.com/go/p/g/1549204213_Raichur-Fort1.jpg"
       alt="First slide"
     />
      <Carousel.Caption>
-      <h3>Ek Minar ki Masjid</h3>
-       <Speech text="Ek Minar ki Masjid is one of the famous mosques in Raichur city of Karnataka. The mosque was built using Persian style of architecture. One of the main attractions of the mosque is its two-storeyed, 20 m high minaret, which can be climbed using a winding staircase. The whole of the town can be viewed from above.
-" />
-    
-      
+      <h3>Raichur Fort</h3>
+       <Speech text="Raichur Fort is a fortress located on a hilltop in the heart of the Raichur in North Karnataka
+       From an inscription on the fort-wall of Raichur, referred to earlier, it is learn that the original fort was built by one Gore Gangayya Reddy, a general of the Kakatiya queen Rudramma Devi of Warangal, in 1294 AD
+       " />
     </Carousel.Caption>
   </Carousel.Item>
+
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://cdn.pixabay.com/photo/2020/08/02/11/20/chennai-to-tirupati-package-5457397__480.jpg"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Ek_Minar_Masjid%2C_Raichur.jpg/780px-Ek_Minar_Masjid%2C_Raichur.jpg"
       alt="Third slide"
     />
-
     <Carousel.Caption>
       <h3>Ek Minar ki Masjid</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <Speech text="Ek Minar ki Masjid is one of the famous mosques in Raichur city of Karnataka. The mosque was built using Persian style of architecture. One of the main attractions of the mosque is its two-storeyed, 20 m high minaret, which can be climbed using a winding staircase. The whole of the town can be viewed from above" />
     </Carousel.Caption>
   </Carousel.Item>
+
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://cdn.pixabay.com/photo/2020/08/02/11/20/chennai-to-tirupati-package-5457397__480.jpg"
+      src="https://www.nativeplanet.com/photos/original/2018/12/photo-92-150920-1.jpg"
       alt="Third slide"
     />
-
     <Carousel.Caption>
       <h3>Tomb of Pir Sailani Shah</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <Speech text="The famous tomb is the place where the Islamic saint – “Pir Sailani Shah”was placed after his death. Begin placed in Raichur, the tomb is a beautifully designed piece of architecture that was built in Bijapur architecture. Driving in a lot of visitors and tourist due to its well architecture and crafty design, one of the tombs main attraction lies in its rectangular shaped hall and dome" />
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://www.nativeplanet.com/photos/original/2016/11/_14782540140.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Markandeshwara Temple</h3>
+      <Speech text="Markandeshwara Temple is located at Kallur near Raichur. The temple is named after the famous Maharishi Markandeya who was an ardent devotee of Lord Shiva. The temple is known for its beautifully carved pillars and other carvings in it" />
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://dynamic.tourtravelworld.com/hotspot-images/jami-masjid-4596.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Jama Masjid</h3>
+      <Speech text="Jama Masjid is one of the largest and most beautiful mosques of Raichur district that boasts of a rich culture and heritage. This huge mosque has two entrances and 6 towering pillars. The ruins of a fort and a palace can be seen near the mosque and a cemetery of Adil Shahi dynasty also can be seen from here" />
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://www.nativeplanet.com/photos/325x244x90/2018/12/photo-92-153911-1.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Manvi</h3>
+      <Speech text="Manvi Surrounded by rocky hills and covered in lush green paddy fields, Manvi is a treat to the eyes. Located in Karnataka’s Raichur district, Manvi has been a historically and religiously significant town for centuries. Forts and temples abound in Manvi, many dating back hundreds of years. The locals of Manvi take great pride in the fact that the great sage and Madhva follower Sri Jagannatha Dasa hails from the small town" />
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://www.nativeplanet.com/photos/929x523x90/2018/12/photo-92-154150-1.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Gabbur</h3>
+      <Speech text="Gabbur is known as the temple town of Raichur district. This place is also known as Gabburu, Manipura, Garbhapura and Gopuragrama. There are around 30 temples and 28 rock edicts, of which most of were built during the reign of Kalyani Chalukyas. Some of the prominent temples here are the Hanuman, the Bangara Basappa, Elu Bhaavi Basavanna, Boodi Basaveshwara Temple, Male Shankara and Venkateshwara. Today, several of these temples are in ruins. During earlier times, Gabbur used to be the centre of education and the place is also linked to Hindu epics like Ramayana and Mahabharata" />
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://www.indianetzone.com/photos_gallery/75/Kallur__Karnataka.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Kallur</h3>
+      <Speech text="Kallur is a well-known archaeological site which is present in Manvi taluk, Raichur District, in the southern Indian state of Karnataka. The site of Kallur achieved fame immediately following the discovery of beautiful 'antennae swords' during the era of 1930's, which were the instances of the culture of 'Copper Hoard' in the southern portion of India. The earliest archaeological evidences unearthed from Kallur can be traced back to the Neolithic Age." />
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
